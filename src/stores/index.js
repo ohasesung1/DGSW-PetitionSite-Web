90b3f52affd194c1be56aa40279c01dba782d93c
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-04-17 10:27:48 +0900

    [SET] develop base setting

---
+++ src/stores/index.js
@@ -0,0 +1,7 @@
+import signStore from './sign';
+
+const stores = {
+  sign: new signStore()
+};
+
+export default stores;
---
+++ src/stores/sign/signStore.js
@@ -0,0 +1,43 @@
+import { action } from 'mobx';
+import { autobind } from 'core-decorators';
+import signRepository from './signRepository';
+
+@autobind
+class signStore {
+
+  @action
+  async handleSignIn (request) {
+    try {
+      const response = await signRepository.handelSignIn(request);
+
+      return new Promise((resolve, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      console.error(error);
+      
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+  }
+
+  @action
+  async handleSignUp (request) {
+    try {
+      const response = await signRepository.handelSignUp(request);
+
+      return new Promise((resolve, reject) => {
+        resolve(response);
+      });
+    } catch (error) {
+      console.error(error);
+
+      return new Promise((resolve, reject) => {
+        reject(error);
+      });
+    }
+  }
+}
+
+export default signStore;
