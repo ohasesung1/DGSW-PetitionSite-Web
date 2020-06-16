7f9846078c863f9d8d6cbc2e7f728574f9ca93fe
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-06-16 13:44:26 +0900

    Update SignInTemplate.js

--- src/components/Sign/SignInTemplate/SignInTemplate.js
+++ src/components/Sign/SignInTemplate/SignInTemplate.js
@@ -18,7 +18,6 @@ const SignInTemplate = ({ idObj, pwObj, userSignIn, isSesstionObj, guestLogin, s
       setIsSesstion(true);
     }
   };
-
   const handleEnter = event => {
     if (event.keyCode === 13) userSignIn();
   };
@@ -34,6 +33,10 @@ const SignInTemplate = ({ idObj, pwObj, userSignIn, isSesstionObj, guestLogin, s
           </div>
           <div className={cx('SignInTemplate-signInContentsBox-pwBox')}>
             <input className={cx('SignInTemplate-signInContentsBox-pwBox-pw')} placeholder={'패스워드'} type={'password'} value={pw} onChange={(event) => setPw(event.target.value)}  onKeyDown={e => handleEnter(e)}/>
+            <input className={cx('SignInTemplate-signInContentsBox-idBox-id')} placeholder={'아이디'} value={id} onChange={(event) => setId(event.target.value)} onKeyPress={(e) => handleKeyPress(e)}/>
+          </div>
+          <div className={cx('SignInTemplate-signInContentsBox-pwBox')}>
+            <input className={cx('SignInTemplate-signInContentsBox-pwBox-pw')} placeholder={'패스워드'} type={'password'} value={pw} onChange={(event) => setPw(event.target.value)} onKeyPress={(e) => handleKeyPress(e)}/>
           </div>
           <div className={cx('SignInTemplate-signInContentsBox-checkBoxDiv')}>
             <span>로그인 유지</span>
