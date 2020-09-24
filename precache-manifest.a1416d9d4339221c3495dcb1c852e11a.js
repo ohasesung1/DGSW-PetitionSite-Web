7c84129134b7f64ddd9dcc74e4f6a022f042f0a5
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-09-24 16:59:23 +0900

    Updates

---
+++ precache-manifest.a1416d9d4339221c3495dcb1c852e11a.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd2dff948b36ff861dfd91a7d8de055",
+    "url": "/DGSW-PetitionSite-Web/index.html"
+  },
+  {
+    "revision": "6cc3180e059aa42849d4",
+    "url": "/DGSW-PetitionSite-Web/static/css/main.7af18cc8.chunk.css"
+  },
+  {
+    "revision": "424b2c8798c8741fa19f",
+    "url": "/DGSW-PetitionSite-Web/static/js/2.649cc9fd.chunk.js"
+  },
+  {
+    "revision": "2922a6766b2d158e439d4c5dadaa0e7b",
+    "url": "/DGSW-PetitionSite-Web/static/js/2.649cc9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc3180e059aa42849d4",
+    "url": "/DGSW-PetitionSite-Web/static/js/main.fb2f8060.chunk.js"
+  },
+  {
+    "revision": "4440eeeed95a6587fa12",
+    "url": "/DGSW-PetitionSite-Web/static/js/runtime-main.15499729.js"
+  },
+  {
+    "revision": "13b04011dde37131099d566b743f351d",
+    "url": "/DGSW-PetitionSite-Web/static/media/Cafe24Ohsquare.13b04011.eot"
+  },
+  {
+    "revision": "142c7208fce7191d968045b7f9ad9c6a",
+    "url": "/DGSW-PetitionSite-Web/static/media/Cafe24Ohsquare.142c7208.woff"
+  },
+  {
+    "revision": "a762326ada249d3f53253644d63fca8b",
+    "url": "/DGSW-PetitionSite-Web/static/media/Cafe24Ohsquare.a762326a.woff2"
+  },
+  {
+    "revision": "e73f200f85489ab73faabc8a3c2ce703",
+    "url": "/DGSW-PetitionSite-Web/static/media/Cafe24Ohsquare.e73f200f.ttf"
+  },
+  {
+    "revision": "1616f04c9c3b347db84b6ce7e6076b77",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Black.1616f04c.woff2"
+  },
+  {
+    "revision": "cc57bd191d7752c4a54f067d0847b10c",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Black.cc57bd19.ttf"
+  },
+  {
+    "revision": "ea7f689df39c56bdb654326b6f5dc601",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Black.ea7f689d.woff"
+  },
+  {
+    "revision": "f277fd81aae229a5e9ff526d369dc868",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Black.f277fd81.eot"
+  },
+  {
+    "revision": "2678db1b847e4faef113835842b8aa1a",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Bold.2678db1b.woff2"
+  },
+  {
+    "revision": "a57f481ab4ece9148af0c0350207dab9",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Bold.a57f481a.eot"
+  },
+  {
+    "revision": "db0986d0e2f29602d8001c84d7bd03cd",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Bold.db0986d0.woff"
+  },
+  {
+    "revision": "e10dd063c818f1426ad3278cce7eb79c",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Bold.e10dd063.ttf"
+  },
+  {
+    "revision": "118e8478906897a1073714a3f1542df0",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Light.118e8478.woff2"
+  },
+  {
+    "revision": "54b0bf04f4285c17d675f33e9be24c91",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Light.54b0bf04.woff"
+  },
+  {
+    "revision": "55e1764ac78314aad18ed3cba90868e3",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Light.55e1764a.ttf"
+  },
+  {
+    "revision": "774efd6b8b953b71d0e9fc8b9d186bb5",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Light.774efd6b.eot"
+  },
+  {
+    "revision": "20e7c007e4a8415739a52e0d3322c3c2",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Medium.20e7c007.woff"
+  },
+  {
+    "revision": "381e7bdf937c34b9bdeed325fee54661",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Medium.381e7bdf.eot"
+  },
+  {
+    "revision": "9ec3449723269a806ec6eaba0f8f6cef",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Medium.9ec34497.ttf"
+  },
+  {
+    "revision": "c9beb953d06c539ddbb44ceb730b83e0",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Medium.c9beb953.woff2"
+  },
+  {
+    "revision": "a863c96f2c0af6d8e3ffb3282799154e",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Regular.a863c96f.eot"
+  },
+  {
+    "revision": "ad582dd370257fc6cb9400ee1fed24cf",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Regular.ad582dd3.woff"
+  },
+  {
+    "revision": "f3e5f298036250d6301103647ace38a4",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Regular.f3e5f298.woff2"
+  },
+  {
+    "revision": "f9493ba9866990680a59ac42835b01bb",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Regular.f9493ba9.ttf"
+  },
+  {
+    "revision": "1e4e110e326a91e8578f48c596b9df16",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-SemiBold.1e4e110e.eot"
+  },
+  {
+    "revision": "390771ca0f5dc56ffc49b2838faf1895",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-SemiBold.390771ca.ttf"
+  },
+  {
+    "revision": "b39023c37438c29f6cc5d6e459007f47",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-SemiBold.b39023c3.woff2"
+  },
+  {
+    "revision": "ec6532d570700c049245927802bd0285",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-SemiBold.ec6532d5.woff"
+  },
+  {
+    "revision": "172af5827f33194727a2f20a3cc94df1",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Thin.172af582.ttf"
+  },
+  {
+    "revision": "368c3e696627d8c28f805fe19afd9b12",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Thin.368c3e69.woff2"
+  },
+  {
+    "revision": "6b450ec7c8423b1aef73945bb554c72e",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Thin.6b450ec7.eot"
+  },
+  {
+    "revision": "ae16defec8ed0fc09b87a6855cd9dee8",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSans-Thin.ae16defe.woff"
+  },
+  {
+    "revision": "03101ca80cf2020b0b813725530f7e0a",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Black.03101ca8.woff2"
+  },
+  {
+    "revision": "72b1daba897aa037cd95b0e426aeebed",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Black.72b1daba.woff"
+  },
+  {
+    "revision": "89efd27c1d18d92e91637390fcdb6d98",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Black.89efd27c.eot"
+  },
+  {
+    "revision": "f6094c02bf3de9e761c6adfa7ae3844d",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Black.f6094c02.ttf"
+  },
+  {
+    "revision": "5fc6f1266e7ea70d652bbdd2ec3a60a8",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Bold.5fc6f126.woff"
+  },
+  {
+    "revision": "6ed888079c1786da8875656c842b75ef",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Bold.6ed88807.eot"
+  },
+  {
+    "revision": "b031c27040b5cc4796801e723041c142",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Bold.b031c270.ttf"
+  },
+  {
+    "revision": "d197a963b8be534ec3620e51addcf533",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Bold.d197a963.woff2"
+  },
+  {
+    "revision": "069d05fdc12f36e11625d728d1d33614",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-DemiLight.069d05fd.woff"
+  },
+  {
+    "revision": "a55cd74b07f21dfcbf9c3bb84ec7e545",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-DemiLight.a55cd74b.woff2"
+  },
+  {
+    "revision": "bca2f0049b39823088e01e6321491bf5",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-DemiLight.bca2f004.ttf"
+  },
+  {
+    "revision": "c4bd5c39dbad9e63cfcd56c9d2018000",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-DemiLight.c4bd5c39.eot"
+  },
+  {
+    "revision": "4c450a82e4271d4aa3184941d4c64444",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Light.4c450a82.ttf"
+  },
+  {
+    "revision": "87f51d248fafffc79156ea28f24f1979",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Light.87f51d24.woff"
+  },
+  {
+    "revision": "9ea09379fe14e88d01db5ddb4c2814d8",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Light.9ea09379.woff2"
+  },
+  {
+    "revision": "f5bfa55680f7839469b718f5a71e5b6b",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Light.f5bfa556.eot"
+  },
+  {
+    "revision": "060168dabab78ea1793ca844bc869bdf",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Medium.060168da.ttf"
+  },
+  {
+    "revision": "69bc289dbb826442729647eddeb84c0d",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Medium.69bc289d.eot"
+  },
+  {
+    "revision": "bfaf1857e71d0b18bac8956117b642df",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Medium.bfaf1857.woff2"
+  },
+  {
+    "revision": "f3b73884150a09ab88bc6954a6e24758",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Medium.f3b73884.woff"
+  },
+  {
+    "revision": "37f99339afe5f99b1dbde5c7ebb89335",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Regular.37f99339.eot"
+  },
+  {
+    "revision": "a22a4546ca1e81aa171a561188d29e50",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Regular.a22a4546.ttf"
+  },
+  {
+    "revision": "af8df214e02587ffb3b131fda847e87d",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Regular.af8df214.woff"
+  },
+  {
+    "revision": "f97524d1b3a0c3b3943493627b7a4fc6",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Regular.f97524d1.woff2"
+  },
+  {
+    "revision": "1123d09ed3f6e0ec77537c2566e8991e",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Thin.1123d09e.woff2"
+  },
+  {
+    "revision": "1977462965985584b575ddf1b07b394f",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Thin.19774629.eot"
+  },
+  {
+    "revision": "c574340424cc0b9229b0109f996e9f14",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Thin.c5743404.woff"
+  },
+  {
+    "revision": "d238a5b563821c4e6f7bdf7059347aca",
+    "url": "/DGSW-PetitionSite-Web/static/media/NotoSansKR-Thin.d238a5b5.ttf"
+  },
+  {
+    "revision": "9ac8a3cce7941a893a0393ae6a5cf5d0",
+    "url": "/DGSW-PetitionSite-Web/static/media/backgroundImage2.9ac8a3cc.jpg"
+  },
+  {
+    "revision": "ccd810f98d2bb54e32df1b9740c1a5de",
+    "url": "/DGSW-PetitionSite-Web/static/media/dgsw_logo.ccd810f9.png"
+  },
+  {
+    "revision": "105dcb1ef12aa402f72c7bf67375f367",
+    "url": "/DGSW-PetitionSite-Web/static/media/schoolImage.105dcb1e.jpg"
+  }
+]);
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Sign } from './Sign';
-export { default as Petition } from './Petition';
-export { default as PetitionWrite } from './PetitionWrite';
-export { default as Admin } from './Admin';
-export { default as PetitionDetail } from './PetitionDetail';
-export { default as StudentCouncil } from './StudentCouncil';
